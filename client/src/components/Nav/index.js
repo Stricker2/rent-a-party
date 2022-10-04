888c3c376a51b7a1d81cb5ca794d6335d09014af
bethanyshoun <bethanyshoun@gmail.com>
2022-10-04 19:17:00 -0400

    updated seeds and small file changes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,41 +5,14 @@ import { Link } from "react-router-dom";
 function Nav() {
 
   function showNavigation() {
-    // if (Auth.loggedIn()) {
-    //   return (
-    //     <ul className="flex-row">
-    //       <li className="mx-1">
-    //         <Link to="/orderHistory">
-    //           Order History
-    //         </Link>
-    //       </li>
-    //       <li className="mx-1">
-    //         {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-    //         <a href="/" onClick={() => Auth.logout()}>
-    //           Logout
-    //         </a>
-    //       </li>
-    //     </ul>
-    //   );
-    // } else {
-      return (
-        <ul className="flex-row">
-            <li className="mx-1">
-                Concessions
-            </li>
-            <li className="mx-1">
-                Tables, Linens, and Chairs
-            </li>
-            <li className="mx-1">
-                Slides
-            </li>
-            <li className="mx-1">
-                Plates, Glassware, and Silverware
-            </li>
-            <li className="mx-1">
-                Tents
-            </li>
-        </ul>
+    return (
+      <ul className="flex-row">
+          <li className="mx-1">
+              <Link to="/contact">
+                  Contact
+              </Link>
+          </li>
+      </ul>
       );
     }
   
@@ -48,7 +21,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="party-hat">🥳</span>
+          <span role="img" aria-label="snow cone">🍧</span>
           Rent-A-Party  
         </Link>
       </h1>
